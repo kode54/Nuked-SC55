bac0d7bc72eb9ae72453044e5e52c90d6b4be67b
Christopher Snowhill <kode54@gmail.com>
2025-09-26 17:26:23 -0700

    Chore: Minor coding changes and cleanup

--- src/mcu.cpp
+++ src/mcu.cpp
@@ -1128,7 +1128,7 @@ void MCU_EncoderTrigger(struct sc55_state *st, int dir)
     MCU_GA_SetGAInt(st, dir == 0 ? 3 : 4, 1);
 }
 
-void MIDI_Reset(struct sc55_state *st, ResetType resetType)
+void MIDI_Reset(struct sc55_state *st, enum ResetType resetType)
 {
     const unsigned char gmReset[] = { 0xF0, 0x7E, 0x7F, 0x09, 0x01, 0xF7 };
     const unsigned char gsReset[] = { 0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7 };
@@ -1150,7 +1150,7 @@ void MIDI_Reset(struct sc55_state *st, ResetType resetType)
 
 }
 
-struct sc55_state * sc55_init(int port, ResetType resetType, sc55_read_rom readCallback, void *readContext)
+struct sc55_state * sc55_init(int port, enum ResetType resetType, sc55_read_rom readCallback, void *readContext)
 {
     struct sc55_state *st = (struct sc55_state *) calloc(1, sizeof(*st));
     if (!st)
@@ -1460,7 +1460,7 @@ void sc55_render(struct sc55_state *st, short *buffer, uint32_t count)
             if (st->sample_buffer_count >= countToDo)
             {
                 sc55_read_samples(st, buffer, count);
-                return;
+                break;
             }
             else
             {
